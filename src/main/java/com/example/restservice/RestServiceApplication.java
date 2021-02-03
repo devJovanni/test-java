e236a596d431f9b2e7a9659b7c372cd535bf39f6
devJovanni <dev@jovanni.services>
2021-02-03 21:57:20 +0000

    Update the pom and Application

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,9 +1,14 @@
 package com.example.restservice;
 
+import com.example.restservice.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
